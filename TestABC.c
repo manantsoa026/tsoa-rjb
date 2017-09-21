4ec3189dfe8c175bc08e2cf10704ef0a8aa741bf
manantsoa <rtianamanantsoa@gmail.com>
2017-09-21 14:52:52 +0300

    Create TestABC.c

---
+++ TestABC.c
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sta.processors.mpo;
+
+/**
+ *
+ * @author ssxa
+ */
+public class TestABC {
+   public static void main(String argv[]){
+       System.out.println("java grand");
+   }    
+}
